cdb667c994f0e2f114b27dd6914206b1033e576a
aluno.lab <aluno.lab@ides.lab>
2024-03-26 17:14:10 -0300

    exeWhile

---
+++ contagem/contagem.js
@@ -0,0 +1,81 @@
+/* let numero = 0;
+
+let contador = 0
+
+while(numero < 51){
+    console.log(numero)
+    numero ++
+
+    console.log("contador " + numero) 
+    contador ++
+
+} */
+
+
+
+
+/* let numeroUsuario = 0
+let contador = 0
+let maiorNumero = 0
+
+while(contador < 10)
+
+    numeroUsuario = Number(prompt("Digite um número:")) //Perguntar o número para o usuário
+
+    if(numeroUsuario > maiorNumero){
+        maiorNumero = numeroUsuario
+        
+        contador ++
+        
+
+
+    }
+
+    alert ("O maior número digitado foi:" + maiorNumero) */
+
+
+
+
+
+
+
+
+/*     let numeroUsuario = 0;
+    let contador = 0;
+    let maiorNumero = 0;
+    let segundoMaior = 0;
+    
+    while(contador < 10){
+    
+        numeroUsuario = Number(prompt('Digite um número:')) // Perguntar o número para o usuário
+       
+        if(numeroUsuario > maiorNumero ){
+            maiorNumero = numeroUsuario
+           
+        }else if(numeroUsuario > segundoMaior){
+            segundoMaior = numeroUsuario
+        }
+    
+        contador ++
+    }
+    
+    alert("O maior número digitado foi: " + maiorNumero + " e o segundo maior é " + segundoMaior)  */
+
+
+    let numeroUsuario = 8
+    let contador = 0
+    
+    while(numeroUsuario != 0){
+        numeroUsuario = prompt("Digite um número ou 0 para encerrar")
+
+        
+        contador ++
+    
+    }
+    
+    console.log(contador - 1)
+
+
+
+
+
